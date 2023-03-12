4184de9795530cff5b22f85dd24a3bebbbd8e112
saiBhandarwad <s333bhandarwad@gmail.com>
2023-03-12 16:33:29 +0530

    update

--- application.py
+++ application.py
@@ -85,5 +85,5 @@ def index():
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0')
+    app.run(host='0.0.0.0',port=5000)
 	#app.run(debug=True)
